9602e8dc1f731ba4d26d3a1ddd0cd77ec33e5821
Kyle Johnson <ktjohnson@student.fullsail.edu>
2019-07-24 00:02:05 -0400

    set up sass

--- gulpfile.js
+++ gulpfile.js
@@ -11,40 +11,40 @@ const server = browserSync.create()
 
 // Set up a helper reference to the various sources we may need throughout the file.
 const src = {
-  scss: 'scss/**/*.scss',
-  css: 'css',
-  html: '*.html',
+    scss: 'scss/**/*.scss',
+    css: 'css',
+    html: '*.html',
 }
 
 // Define our CSS processing task
 function css() {
-  // When using gulp in a task, it is important to return the result
-  return (
-    gulp
-      // Take in the scss source files via gulp.src()
-      .src(src.scss)
-      // Pipe our source file content through the sass function
-      .pipe(sass().on('error', sass.logError))
-      // Pipe the transformed file content into the gulp.dest() method to write the file out.
-      .pipe(gulp.dest(src.css))
-  )
+    // When using gulp in a task, it is important to return the result
+    return (
+        gulp
+        // Take in the scss source files via gulp.src()
+            .src(src.scss)
+            // Pipe our source file content through the sass function
+            .pipe(sass().on('error', sass.logError))
+            // Pipe the transformed file content into the gulp.dest() method to write the file out.
+            .pipe(gulp.dest(src.css))
+    )
 }
 
 // Define a reusable reload task for browsersync to use
 // This task acts as a way to tell the server that changes have been made
 function reload(done) {
-  server.reload()
-  done()
+    server.reload()
+    done()
 }
 
 // Define our serve task to start up the browsersync server
 function serve(done) {
-  server.init({
-    server: {
-      baseDir: './',
-    },
-  })
-  done()
+    server.init({
+        server: {
+            baseDir: './',
+        },
+    })
+    done()
 }
 
 // Define our watch task (this is using a shorthand arrow function syntax)
